04b5ddfa20e8a9a0c2990cf98d24e5cdd81fa7d9
David Garcia <david@techdocs.studio>
2025-11-04 16:22:49 +0000

    docs: add redirection warning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,6 +142,7 @@ sitemap_url_scheme = '/stable/{link}'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_scylladb_theme'
+templates_path = ['_templates', ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
